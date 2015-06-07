b6fb5561455c17df2c1b34e73035d603c14698e4
Josiah Gaskin <josiahgaskin@gmail.com>
2015-06-07 14:40:52 -0700

    Added accept/deny to request model

--- Pretto/Request.swift
+++ Pretto/Request.swift
@@ -11,10 +11,14 @@ import Foundation
 private let kClassName = "Request"
 private let kPhotoKey = "photo"
 private let kRequesterKey = "requester"
+private let kRequestStatusKey = "status"
 
 class Request : BackendObject {
-    override init() {
+    init(photo: ThumbnailPhoto) {
         super.init(className: kClassName)
+        self.status = "pending"
+        self.photo = photo
+        self.requester = BackendUser.currentUser()
     }
     
     var photo : ThumbnailPhoto? {
@@ -23,7 +27,36 @@ class Request : BackendObject {
     }
     
     var requester : BackendUser? {
-        set { setValue(requester, forKey: kRequesterKey) }
+        set(oldValueOrNil) {
+            setValue(requester, forKey: kRequesterKey)
+            if let oldValue = oldValueOrNil {
+                self.ACL?.setWriteAccess(false, forUser: oldValue)
+            }
+            if let newValue = requester {
+                self.ACL?.setWriteAccess(true, forUser: newValue)
+            }
+        }
         get { return self[kRequesterKey] as? BackendUser ?? nil }
+    }
+    
+    var status : String? {
+        set { setValue(status, forKey: kRequestStatusKey) }
+        get { return self[kRequestStatusKey] as? String ?? nil }
+    }
+    
+    func acceptRequest() {
+        if let desiredPhoto = self.photo?.fullsizePhoto?.fetchIfNeeded() {
+            if self.requester != nil {
+                desiredPhoto.ACL?.setReadAccess(true, forUser: self.requester!)
+                self.status = "accepted"
+                desiredPhoto.saveInBackgroundWithBlock(nil)
+                self.saveInBackgroundWithBlock(nil)
+            }
+        }
+    }
+    
+    func denyRequest() {
+        self.status = "denied"
+        self.saveInBackgroundWithBlock(nil)
     }
 }
