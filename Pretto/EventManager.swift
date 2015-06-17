c444bc8b1cc5e26de52aa4d4898fb198a624a5b0
Francisco <franciscodlp@gmail.com>
2015-06-17 03:37:47 -0700

    Test Auxiliary Class EventManager

--- Pretto/EventManager.swift
+++ Pretto/EventManager.swift
@@ -6,6 +6,9 @@
 //  Copyright (c) 2015 Pretto. All rights reserved.
 //
 
+
+
+// DO NOT PAY ATTENTION TO THIS. THIS CLASS IS ONLY FOR TESTING
 import UIKit
 
 
@@ -13,7 +16,25 @@ class EventManager: NSObject {
     
     typealias createEventCompletion = (success: Bool, userInvitation: Invitation, intitationsSent: [Invitation]?) -> ()
     
+    
+    
     func createEvent(dictionary: NSDictionary, completion: createEventCompletion) {
+        //Create Event and the associated Album. It aslo create (calls XXXX) and send Invitation objects (calls XXXX ) for every Guest.
+    }
+    
+    func createAlbumForEvent(event: Event) {
+        
+    }
+    
+    func createOwnerInvitationForEvent(event: Event, owner: User) {
+        
+    }
+    
+    func createGuestInviationForEvent(event: Event, guest: User) {
+        
+    }
+    
+    func createGuestInviationsWithArray(event: Event, guests: [User]) {
         
     }
 
